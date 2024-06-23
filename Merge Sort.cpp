8d7d02dad441822b1eaaeea26ff3ea34318ab455
Towsif Mahabub <147875322+Towsifsnm@users.noreply.github.com>
2024-06-23 22:46:03 +0600

    Update Merge Sort.cpp

--- Merge Sort.cpp
+++ Merge Sort.cpp
@@ -45,7 +45,7 @@ void merge(int arr[], int left, int mid, int right) {
 
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
-        int mid = left + (right - left) / 2;
+        int mid = left + right/ 2;
 
 
         mergeSort(arr, left, mid);
